45583b42a186bfeb40dac9e92daa3196e56a0aa3
yinm <yinm.cminor9@gmail.com>
2018-07-24 20:43:53 +0900

    Add createAsset()

--- src/minipack.js
+++ src/minipack.js
@@ -5,3 +5,33 @@ const traverse = require('babel-traverse').default
 const { transformFromAst } = require('babel-core')
 
 let ID = 0
+
+function createAsset(filename) {
+  const content = fs.readFileSync(filename, 'utf-8')
+
+  const ast = babylon.parse(content, {
+    sourceType: 'module',
+  })
+
+  const dependencies = []
+
+  traverse(ast, {
+    ImportDeclaration: ({node}) => {
+      dependencies.push(node.source.value)
+    }
+  })
+
+  const id = ID++
+
+  const {code} = transformFromAst(ast, null, {
+    presets: ['env'],
+  })
+
+  return {
+    id,
+    filename,
+    dependencies,
+    code,
+  }
+}
+
