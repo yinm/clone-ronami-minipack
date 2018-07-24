1db2e7f08275b3d8c5b3f3d0f7feb2a6297717db
yinm <yinm.cminor9@gmail.com>
2018-07-24 21:06:28 +0900

    Add bundle()

--- src/minipack.js
+++ src/minipack.js
@@ -54,3 +54,37 @@ function createGraph(entry) {
   return queue
 }
 
+function bundle(graph) {
+  let modules = ''
+
+  graph.forEach(mod => {
+    modules += `${mod.id}: [
+      function (require, module, exports) {
+        ${mod.code}
+      },
+      ${JSON.stringify(mod.mapping)},
+    ]`
+  })
+
+  const result = `
+    (function(modules) {
+      function require(id) {
+        const [fn, mapping] = modules[id]
+        
+        function localRequire(name) {
+          return require(mapping[name])
+        } 
+        
+        const module = { exports: {} }
+        
+        fn(localRequire, module, module.exports)
+        
+        return module.exports
+      }
+      
+      require(0)
+    })({${modules}})
+  `
+
+  return result
+}
